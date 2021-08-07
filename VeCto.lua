a0915a15885186e9a35fa1e926b9248baec3dc1a
TEAMVEctO <84635710+TEAMVEctO@users.noreply.github.com>
2021-08-07 19:25:18 +0300

    Add files via upload

--- VeCto.lua
+++ VeCto.lua
@@ -8583,10 +8583,7 @@ send(msg.chat_id_, msg.id_, "* ゠⁞ تم التحديث*")
 end
 
 if text == 'السورس' or text == 'سورس' or text == 'ياسورس' or text == 'يا سورس' then  
-local url,res = https.request('https://VEctO-source.tk/VEctOTeAM/ChatMember.php?id='..msg.sender_user_id_)
-data = JSON.decode(url)
-if data.Ch_Member.info ~= true then
-send(msg.chat_id_,msg.id_,'⌔︙شترك في قناة السورس اولآ @fBBBBB .')   
+send(msg.chat_id_,msg.id_,'⌔︙شترك في قناة السورس اولآ @TeAm_VeCto .')   
 return false 
 end
 Text = [[
