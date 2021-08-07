f8b80edc8ff95eb3a232329b257496e9c60209b2
TEAMVEctO <84635710+TEAMVEctO@users.noreply.github.com>
2021-08-07 23:53:29 +0300

    Add files via upload

--- start.lua
+++ start.lua
@@ -73,11 +73,11 @@ RunBot:write([[
 #!/usr/bin/env bash
 cd $HOME/VEctO
 token="]]..database:get(Server_Done.."Token_Write")..[["
-rm -fr VeCto.lua
-wget "https://raw.githubusercontent.com/TEAMVEctO/VEctO/main/VeCto.lua"
+rm -fr VEctO.lua
+wget "https://raw.githubusercontent.com/TEAMVEctO/VEctO/main/VEctO.lua"
 while(true) do
 rm -fr ../.telegram-cli
-./tg -s ./VeCto.lua -p PROFILE --bot=$token
+./tg -s ./VEctO.lua -p PROFILE --bot=$token
 done
 ]])
 RunBot:close()
