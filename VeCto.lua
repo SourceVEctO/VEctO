2ed1563d7b9a110074f8646a6226a068087b6869
TEAMVEctO <84635710+TEAMVEctO@users.noreply.github.com>
2021-08-09 15:07:25 +0300

    Add files via upload

--- VeCto.lua
+++ VeCto.lua
@@ -8243,16 +8243,15 @@ https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. 
 end
 end
 end
-
 if text == "غنيلي" then
-data,res = https.request('https://VEctO-source.tk/VEctO TeAm/audios.php')
+data,res = https.request('https://Black-source.tk/BlackTeAM/audios.php')
 if res == 200 then
 audios = json:decode(data)
 if audios.Info == true then
-local Text ='* ゠⁞ تم اختيار المقطع الصوتي لك*'
+local Text ='゠⁞ تم اختيار المقطع الصوتي لك'
 keyboard = {} 
 keyboard.inline_keyboard = {
-{{text = '- VeCto TeAM .',url="t.me/TEAM_VEctO"}},
+{{text = '- VeCto TeAM .',url="t.me/TeAM_VEctO"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendVoice?chat_id=' .. msg.chat_id_ .. '&voice='..URL.escape(audios.info)..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
