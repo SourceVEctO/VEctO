d988bdef88cdc22b8daddd4d5026acca71a68e3d
TEAMVEctO <84635710+TEAMVEctO@users.noreply.github.com>
2021-08-11 02:43:51 +0300

    Add files via upload

--- VeCto.lua
+++ VeCto.lua
@@ -8689,19 +8689,15 @@ Text = [[
  
 [ ゠⁞ Ch Source](t.me/TEAM_VEctO) 
 ]]
-send(msg.chat_id_, msg.id_,Text)
-return false
-end
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '⓵', callback_data="/help1"},{text = '⓶', callback_data="/help2"},{text = '⓷', callback_data="/help3"}},
 {{text = '⓸', callback_data="/help4"},{text = '⓹', callback_data="/help5"}},
 }
-local msg_id = msg.id_
+local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
 end
-
 
 
 if text == 'م1' and Addictive(msg) then  
