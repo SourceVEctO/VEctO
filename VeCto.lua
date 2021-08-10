f8ae8c6bbbd98d14283a8957f65900dfebb2ab40
TEAMVEctO <84635710+TEAMVEctO@users.noreply.github.com>
2021-08-10 15:07:31 +0300

    Add files via upload

--- VeCto.lua
+++ VeCto.lua
@@ -5396,9 +5396,10 @@ send(msg.chat_id_, msg.id_,'['..TextReply..']')
 return false
 end
 
+
 if text == 'مطورسورس' or text == "مطور السورس" or text == "مطور سورس" then 
 
-Text =  "゠⁞  Name : 𝗗ِ𝗮ٌِ𝗥ً𝗞 .
+Text = "゠⁞  Name : 𝗗ِ𝗮ٌِ𝗥ً𝗞 .
 ゠⁞ Dev User : @mmssds"  
 keyboard = {} 
 keyboard.inline_keyboard = {
@@ -5407,7 +5408,7 @@ keyboard.inline_keyboard = {
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/mmssds&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
-end
+
 
 
 if text == "الاوامر المضافه" and Constructor(msg) then
